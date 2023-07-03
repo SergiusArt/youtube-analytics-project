0fb702e39e146a1ed8948bff51eb5636bc92108b
Sergius Art <xcxserj@mail.ru>
2023-07-03 20:24:24 +0300

    Добавлены в инициализатор новые атрибуты с данными канала, реализован класс-метод, возвращающий объект для работы с YouTube, данные записываются в json-файл

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+import json
 import os
 from googleapiclient.discovery import build
 
@@ -6,16 +7,49 @@ class Channel:
     """Класс для ютуб-канала"""
 
     api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+
+        self.__channel_id = channel_id
+        self.channel = Channel.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+
+        self.id = self.channel['items'][0]['id']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.custom_url = self.channel['items'][0]['snippet']['customUrl']
+        self.url = f'https://www.youtube.com/{self.custom_url}'
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        """
+        Позволяет обращаться к channel ID как к аргументу
+        """
+
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        """ Возвращает объект для работы с TouTube API"""
+
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+    def to_json(self, path):
+        """ Сохраняет в файл значения атрибутов экземпляра """
+        data = self.__dict__
+        del data['channel']
+        with open(path, 'w') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
 
     def __repr__(self):
         """ Выводит ID канала к которому привязан класс и наличие соединения по АПИ"""
+
         return f'ID канала = {self.channel_id}, API ключ = {len(self.api_key) > 1}'
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
+
         print(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute())
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -9,3 +9,16 @@ def class_channel():
 
 def test_repr(class_channel):
     assert class_channel.__repr__() == f'ID канала = UC-OVMPlMA3-YCIeg4z5z23A, API ключ = True'
+
+
+def test_init(class_channel):
+    assert class_channel.channel_id == 'UC-OVMPlMA3-YCIeg4z5z23A'
+    assert class_channel.id == "UC-OVMPlMA3-YCIeg4z5z23A"
+    assert class_channel.title == "MoscowPython"
+    assert class_channel.description == "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\n" \
+                                        "Присоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)"
+    assert class_channel.custom_url == "@moscowdjangoru"
+    assert class_channel.url == 'https://www.youtube.com/@moscowdjangoru'
+    assert class_channel.subscriber_count > 0
+    assert class_channel.video_count > 0
+    assert class_channel.view_count > 0
